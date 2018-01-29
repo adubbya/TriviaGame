261964107fe2265a61dc0e3959c40c4232246863
adubbya <aaron.mitchell@austin.utexas.edu>
2018-01-29 02:11:36 -0600

    Initial Buildup 2, last repo died

---
+++ assets/javascript/app.js
@@ -0,0 +1,22 @@
+// click a start button to start the game
+// timer begins counting down
+// display timer that counts down
+// display questions and possible answers on screen
+// use radios to select possible answer per question 
+// after user selects answers, click done to end game
+// timer ends the game at timeout
+//end game screen pops up to display number of right, wrong, and unanswered
+
+// VARIABLES
+
+// DOC READY
+
+// GAME START
+
+//ON CLICK 
+
+// FUNCTIONS
+
+// TIMER
+
+// GAME END
